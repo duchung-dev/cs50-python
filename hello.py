a088d61c9bb7df07e3f7c045362d48af105bf705
root <root@LAPTOP-JU47EF9I.localdomain>
2025-12-10 18:40:28 +0800

    Add lecture cs50

--- hello.py
+++ hello.py
@@ -1,8 +1,2 @@
-# Ask user for their name and greet them
-name = input("what's your name? ").strip().title()
-
-# Split the name into parts (first and last)
-first, last = name.split(" ")
-# Greet the user
-print(f"hello, {first}")
-
+print("hello, world ")
+input("Press Enter to exit...")
