1777f53ac047569c7edad340c48dc117eba3c0c3
root <root@LAPTOP-JU47EF9I.localdomain>
2025-12-25 10:32:39 +0800

    add new change

--- hello.py
+++ hello.py
@@ -1,3 +1,14 @@
-name = input ("what's your name?").strip().title()
-first, last = name.split(" ")
-print(f"hello, {first}")
+score = int(input("Score: "))
+
+if score >= 85:
+    print ("Grade: HD")
+elif score >= 75:
+    print ("Grade: D")
+elif score >= 65:
+    print ("Grade: C")
+elif score >= 50:
+    print ("Grade: P")
+elif score >= 30:
+    print ("Grade: F")
+else:
+    print ("Grade: FF")
