e235f4c4b6386cc2a987649d5c69cc7202374006
duchung-dev <ceophamduchung01@gmail.com>
2025-12-28 20:52:43 +0800

    add remove file

--- Week1/grade.py
+++ Week1/grade.py
@@ -1,14 +1,14 @@
 score = int(input("Score: "))
 
-if   85 <= score <= 100:
+if   score >= 85:
     print("Grade: HD")
-elif 75 <= score <= 84:
+elif score >= 75:
     print("Grade: D")
-elif 65 <= score <= 74:
+elif score >= 65:
     print("Grade: C")
-elif score >= 50 and score <= 64:
+elif score >= 50:
     print("Grade: P")
-elif score >= 30 and score <= 49:
+elif score >= 30:
     print("Grade: F")
 else:
     print("Grade: FF")
