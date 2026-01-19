74eb22d533143cd08b32e555e4f80c9b11b3d498
duchung-dev <ceophamduchung01@gmail.com>
2026-01-19 21:26:16 +0800

    add

--- Week3/practice.py
+++ Week3/practice.py
@@ -3,9 +3,11 @@
 
 # Demonstrates division
 # Minh họa phép chia
-x = float(input("What's x? "))
-y = float(input("What's y? "))
+x = int(input("What's x? "))
+y = int(input("What's y? "))
 
-z = x / y
+# Rounds to 2 decimal places
+# Làm tròn đến 2 chữ số thập phân
+z = round(x / y, 2)
 
 print(z)
