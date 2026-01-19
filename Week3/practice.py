4eabdc9e6fd8cce80bac1946558183ff04b51402
duchung-dev <ceophamduchung01@gmail.com>
2026-01-19 20:57:01 +0800

    add

--- Week3/practice.py
+++ Week3/practice.py
@@ -1,11 +1,11 @@
-# Demonstrates rounding to nearest int
-# Minh họa làm tròn đến số nguyên gần nhất
+# EX: Demonstrates rounding to nearest int
+# VD: Minh họa làm tròn đến số nguyên gần nhất
 
-# Demonstrates fewer variables
-# Minh họa sử dụng ít biến hơn
-
-
+# Demonstrates division
+# Minh họa phép chia
 x = float(input("What's x? "))
 y = float(input("What's y? "))
 
-z = print(round(x + y))
+z = x / y
+
+print(z)
