460f13059b1318ead1509a95fa030386d177d545
duchung-dev <ceophamduchung01@gmail.com>
2026-01-19 21:34:43 +0800

    add

--- Week3/practice.py
+++ Week3/practice.py
@@ -3,11 +3,11 @@
 
 # Demonstrates division
 # Minh họa phép chia
-x = int(input("What's x? "))
-y = int(input("What's y? "))
 
-# Rounds to 2 decimal places
-# Làm tròn đến 2 chữ số thập phân
-z = round(x / y, 2)
+def hello(to="world"):
+    print("hello,", to)
 
-print(z)
+hello()
+name = input("What's your name? ")
+hello(name)
+
