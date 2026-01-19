243cf1b0a09333ed8f9ba5acd207ff0845435344
duchung-dev <ceophamduchung01@gmail.com>
2026-01-19 21:54:19 +0800

    add

--- Week3/practice.py
+++ Week3/practice.py
@@ -1,13 +1,14 @@
 # EX: Demonstrates rounding to nearest int
 # VD: Minh họa làm tròn đến số nguyên gần nhất
 
-# Demonstrates division
-# Minh họa phép chia
+# Demonstrates defining a function with a return value
+# Minh họa định nghĩa hàm với giá trị trả về
 
-def hello(to="world"):
-    print("hello,", to)
+def main ():
+    x = int(input("What's x? "))
+    print("x squared is", square(x))
 
-hello()
-name = input("What's your name? ")
-hello(name)
+def square(n):
+    return n * n
 
+main()
