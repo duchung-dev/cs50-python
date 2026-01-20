32494a82930429b2ba84ad5edfbbbd7ef15aed20
duchung-dev <ceophamduchung01@gmail.com>
2026-01-20 10:51:47 +0800

    Add practice1.py

---
+++ Week3/practice1.py
@@ -0,0 +1,12 @@
+# Demonstrates conditionals
+# Minh họa các câu lệnh điều kiện 
+
+x = int(input("What's x? "))
+y = int(input("What's y? "))
+
+if x < y:
+    print("x is less than y")
+if x > y:
+    print("x is greater than y")
+if x == y:
+    print("x is equal to y")
