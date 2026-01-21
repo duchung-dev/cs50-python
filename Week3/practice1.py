514e542b8bd600affbb6e3595ffbc5245fa1427f
duchung-dev <ceophamduchung01@gmail.com>
2026-01-21 19:22:52 +0800

    add

--- Week3/practice1.py
+++ Week3/practice1.py
@@ -1,8 +1,12 @@
 # Demonstrates conditionals
 # Minh họa các câu lệnh điều kiện 
 
-students = ["Hermione", "Harry", "Ron"]
+x = int(input("What's x? "))
+y = int(input("What's y? "))
 
-for student in students:
-
-    print(student)
+if x < y:
+    print("x is less than y")
+elif x > y:
+    print("x is greater than y")
+elif x == y:
+    print("x is equal to y")
