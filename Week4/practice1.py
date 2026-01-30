9168f0e56f84a025616d8a40f2b024185402f756
duchung-dev <hieunguyentn0402@gmail.com>
2026-01-30 23:39:21 +0800

    Add practice

---
+++ Week4/practice1.py
@@ -0,0 +1,7 @@
+# Demomnstrates import and random.choice
+# Minh họa việc nhập khẩu và random.choice
+
+import random
+
+coin = random.choice(["heads", "tails"])
+print(coin)
---
+++ Week4/practice2.py
@@ -0,0 +1,7 @@
+# Demonstrates from
+# Mô tả cách sử dụng từ khóa `from`
+
+from random import choice
+
+coin = choice(['Heads', 'Tails'])
+print(coin)
