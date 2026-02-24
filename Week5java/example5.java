cc772321f9d1abc5df9c547fa1d0e5ab0769f7c2
duchung-dev <hieunguyentn0402@gmail.com>
2026-02-24 09:08:47 +0800

    add

---
+++ Week5java/example5.java
@@ -0,0 +1,13 @@
+public class GradeExample {
+    public static void main(String[] args) {
+        int num = 100;
+        char letter = 'W';
+        boolean flag = true;
+        String name = "Hello, Amit!";
+
+        System.out.println("Number: " + num);
+        System.out.println("Letter: " + letter);
+        System.out.println("Flag: " + flag);
+        System.out.println("Name: " + name);
+        }
+    }
